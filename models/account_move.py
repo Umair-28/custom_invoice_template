44721003c92a673ade0e6c9a03ef867a97680749
Umair-28 <h.umairamjad.30@gmail.com>
2025-11-24 12:23:02 +0500

    added more fields

--- models/account_move.py
+++ models/account_move.py
@@ -7,4 +7,13 @@ class AccountMove(models.Model):
     mode_of_payment = fields.Char(string="Mode of Payment")
     ref_no = fields.Char(string="Reference No")
     other_ref = fields.Char(string="Other References")
+    
+    buyer_order_no = fields.Char(string="Buyer Order No")
+    dispatch_doc_no = fields.Char(string="Dispatch Doc No", related="name")
+    delivery_note_date = fields.Char(string="Delivery Note Date")
+    dispatch_through = fields.Char(string="Dispatched Through")
+    destination = fields.Char(string="Destination")
+    bill_of_landing = fields.Char(string="Bill of Landing/LR-RR-No")
+    vehicle_no = fields.Char(stirng="Vehicle No")
+    delivery_terms = fields.Char(string="Terms of Delivery")
 

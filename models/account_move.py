d6874c0ce49f80b61538be2b6c9932fa533c0826
Umair-28 <h.umairamjad.30@gmail.com>
2025-11-24 12:49:20 +0500

    update xml fields

--- models/account_move.py
+++ models/account_move.py
@@ -7,9 +7,14 @@ class AccountMove(models.Model):
     mode_of_payment = fields.Char(string="Mode of Payment")
     ref_no = fields.Char(string="Reference No")
     other_ref = fields.Char(string="Other References")
-    
+
     buyer_order_no = fields.Char(string="Buyer Order No")
-    dispatch_doc_no = fields.Char(string="Dispatch Doc No", related="name")
+    dispatch_doc_no = fields.Char(
+        string="Dispatch Doc No",
+        related="name",
+        store=True,
+        readonly=False
+    )
     delivery_note_date = fields.Char(string="Delivery Note Date")
     dispatch_through = fields.Char(string="Dispatched Through")
     destination = fields.Char(string="Destination")
